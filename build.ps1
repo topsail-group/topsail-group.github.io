bf55efd97be828ed98d8883ac4d460b18d02ce98
CountOfDarkness <105372031+CountOfDarkness@users.noreply.github.com>
2023-03-22 13:15:10 +0200

    fr version added

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ $path = (pwd).Path
 $targetDir = "$path\docs"
 $sourceDir = "$path\www"
 Remove-Item $targetDir -Recurse
-cmd.exe /c "static-i18n -o docs -i en -i lv -i ru www"
+cmd.exe /c "static-i18n -o docs -i en -i lv -i ru -i fr www"
 Copy-Item -Path "$sourceDir\css" -Destination "$targetDir\css" -Recurse
 Copy-Item -Path "$sourceDir\js" -Destination "$targetDir\js" -Recurse
 Copy-Item -Path "$sourceDir\img" -Destination "$targetDir\img" -Recurse
