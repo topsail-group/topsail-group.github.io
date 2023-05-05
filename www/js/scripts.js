6545d7d78ef1455546e2e543dc77d204b248d9ef
CountOfDarkness <105372031+CountOfDarkness@users.noreply.github.com>
2023-05-05 12:51:41 +0300

    top-navbar-collapse behaviour removed

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -20,17 +20,6 @@
 		hidePreloader();
 	});
 
-	
-	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 60) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
-    });
-
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
 		$(document).on('click', 'a.page-scroll', function(event) {
